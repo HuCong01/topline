d5bd9bb34e735f475b9d9202f13bdc1eb8927daa
hc <2502611416@qq.com>
2020-01-05 20:18:21 +0800

    安装element-ui组件库

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'// 引入elementui组件库
+// 把组件库注册给Vue
+// 之前组件注册：Vue.component(名称,组件) 每次只能注册一个
+// 现在组件注册: Vue.use(组件模块) 一次性注册"全部"的组件，非常高效
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
