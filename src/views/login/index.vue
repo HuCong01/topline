44eda122aa3a20386714c78566c5d794dd43fe05
hc <2502611416@qq.com>
2020-01-08 11:24:51 +0800

    登录功能开发完毕

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,31 +1,46 @@
 <template>
-    <div class="login-container">
-      <div class="login-box">
-          <!-- 绘制登录form表单 -->
-           <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules">
-          <img src="./index.png" alt="">
-          <el-form-item prop="mobile">
-            <el-input v-model="loginForm.mobile" placeholder="请输入手机号码"></el-input>
-          </el-form-item>
-          <el-form-item prop="code">
-            <el-input v-model="loginForm.code" placeholder="请输入验证码"></el-input>
-          </el-form-item>
-          <el-form-item style="text-align:left;" prop="xieyi">
-            <el-checkbox v-model="loginForm.xieyi" style="margin-right:10px;"></el-checkbox>
-            <span>我已阅读并同意 <a href="#">用户协议</a> 和 <a href="#">隐私条款</a> </span>
-          </el-form-item>
-          <el-form-item>
-            <el-button style="width:100%;" type="primary" @click="login()">登录</el-button>
-          </el-form-item>
-          </el-form>
-      </div>
+  <div class="login-container">
+    <div class="login-box">
+      <!-- 绘制登录form表单 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules">
+        <img src="./index.png" alt />
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号码">
+             <i slot="prefix" class="iconfont icon-smartphone"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码">
+            <i slot="prefix" class="iconfont icon-icon_verification"></i>
+          </el-input>
+        </el-form-item>
+        <el-form-item style="text-align:left;" prop="xieyi">
+          <el-checkbox v-model="loginForm.xieyi" style="margin-right:10px;"></el-checkbox>
+          <span>
+            我已阅读并同意
+            <a href="#">用户协议</a> 和
+            <a href="#">隐私条款</a>
+          </span>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            style="width:100%;"
+            type="primary"
+            @click="login()"
+            :loading="isActive"
+            :disabled="isActive"
+          >登录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
 import './gt.js'
+// 引入iconfont的css文件
+import '../../assets/iconfont/iconfont.css'
 export default {
-
   name: '',
   data () {
     // 自定义校验函数，验证协议
@@ -52,17 +67,14 @@ export default {
           { required: true, message: '手机号码必填' },
           { pattern: /^1[35789]\d{9}$/, message: '手机号码格式不对' }
         ],
-        code: [
-          { required: true, message: '验证码必填' }
-        ],
-        xieyi: [
-          { validator: xieyiTest }
-        ]
-      }
+        code: [{ required: true, message: '验证码必填' }],
+        xieyi: [{ validator: xieyiTest }]
+      },
+      isActive: false,
+      ctaObj: null // 极验对象
     }
   },
   methods: {
-
     // 登录系统
     login () {
       // 全部表单域项目校验
@@ -70,7 +82,16 @@ export default {
       // form组件.validate(回调函数)
       // 参数valid：会体现布尔值，表示校验是否成功
       this.$refs.loginFormRef.validate(valid => {
-        if (!valid) { return false }
+        if (!valid) {
+          return false
+        }
+        // 表单校验成功
+
+        if (this.ctaObj !== null) {
+          return this.ctaObj.verify()
+        }
+        this.isActive = true
+
         // A.人机交互验证
         // axios获得极验的密匙信息
         let pro = this.$http({
@@ -81,25 +102,31 @@ export default {
           .then(result => {
             // console.log(result)//获取极验秘钥信息
             // 从result里边解构下述的data对象出来
-            let{ data } = result.data
+            let { data } = result.data
             // 请检测data的数据结构，保证data.gt,data.challenge,data.success有值
-            window.initGeetest({
-            // 以下配置来自服务端 SDK
-              gt: data.gt,
-              challenge: data.challenge,
-              offline: !data.success,
-              new_captcha: true,
-              product: 'bind' // 设置验证窗口样式的
-            }, captchaObj => {
-              // 这里可以调用验证实例 captchaObj 的实例方法
-              captchaObj.onReady(() => {
-                captchaObj.verify()// 显示验证码窗口
-              }).onSuccess(() => {
-                this.loginAct()
-              }).onError(() => {
-
-              })
-            })
+            window.initGeetest(
+              {
+                // 以下配置来自服务端 SDK
+                gt: data.gt,
+                challenge: data.challenge,
+                offline: !data.success,
+                new_captcha: true,
+                product: 'bind' // 设置验证窗口样式的
+              },
+              captchaObj => {
+                // 这里可以调用验证实例 captchaObj 的实例方法
+                captchaObj
+                  .onReady(() => {
+                    captchaObj.verify() // 显示验证码窗口
+                    this.isActive = false
+                    this.ctaObj = captchaObj // 已经极验对象赋予给ctaObj
+                  })
+                  .onSuccess(() => {
+                    this.loginAct()
+                  })
+                  .onError(() => {})
+              }
+            )
           })
           .catch(err => {
             return this.$message.error('获取极验密匙失败:' + err)
@@ -118,7 +145,10 @@ export default {
         .then(result => {
           // console.log(result) // 【data】  status statusText headers config request
           // 客户端浏览器把服务器端返回的秘钥等相关信息通过 sessionStorage 做记录，表明是登录状态
-          window.sessionStorage.setItem('userinfo', JSON.stringify(result.data.data))
+          window.sessionStorage.setItem(
+            'userinfo',
+            JSON.stringify(result.data.data)
+          )
           this.$router.push({ name: 'home' })
         })
         .catch(err => {
@@ -139,29 +169,29 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.login-container{
-  height:100%;
+.login-container {
+  height: 100%;
   background-color: gray;
   background-image: url("./big.jpg");
   background-size: cover;
-  display:flex;
+  display: flex;
   justify-content: center;
   align-items: center;
-  .login-box{
-    width:410px;
-    height:345px;
+  .login-box {
+    width: 410px;
+    height: 345px;
     background-color: rgba(0, 255, 255, 0.5);
-    display:flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  img{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    img {
       height: 70px;
       width: 50%;
       margin: 20px auto;
-  }
-    .el-form{
-        width: 75%;
+    }
+    .el-form {
+      width: 75%;
     }
   }
 }
